8e5f05f3e5e6425846af6ef2bb51b79f27456d5e
tejas96 <tejas96patil@gmail.com>
2021-11-22 01:14:56 +0530

    added .graphql schema file, created scalable developer friendly scalable resolver, setup apollo auth server & setup firebase admin

--- packages/auth-service/authService.js
+++ packages/auth-service/authService.js
@@ -1,25 +1,12 @@
 import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
 import mongoose from 'mongoose';
+import { ApolloServer, gql } from 'apollo-server-express';
+import fs from 'fs';
+import path from 'path';
+import resolveQuery from './src/controller';
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const app = express();
-
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-var root = {
-    hello: () => {
-        return 'Hello world!';
-    },
-};
 
 mongoose
     .connect(process.env.DB_CONNECTION_URL || '', {
@@ -33,14 +20,27 @@ mongoose
         console.log(error);
     });
 
-app.use(
-    '/graphql',
-    graphqlHTTP({
-        schema: schema,
-        rootValue: root,
-        graphiql: true,
-    })
-);
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}/graphql`);
-});
+//function that imports .graphql files
+const importGraphQLAndGetSchema = (file) => {
+    const schema = fs.readFileSync(path.join(__dirname, file), 'utf-8');
+    return gql`
+        ${schema}
+    `;
+};
+
+const resolvers = resolveQuery();
+
+async function createServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs: importGraphQLAndGetSchema('./src/.graphql'),
+        resolvers,
+    });
+    await server.start();
+    server.applyMiddleware({ app });
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+createServer();
