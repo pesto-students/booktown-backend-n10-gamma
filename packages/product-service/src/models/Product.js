82f65fc9cafe436612e27a49bfedef36204bb367
tejas96 <tejas96patil@gmail.com>
2021-11-29 18:59:01 +0530

    create product mongoose schema

---
+++ packages/product-service/src/models/Product.js
@@ -0,0 +1,68 @@
+import Mongoose from 'mongoose';
+
+const ProductSchema = new Mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    files: {
+        type: [String],
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    subcategory: {
+        type: String,
+        required: true,
+    },
+    shippingAddress: {
+        city: {
+            type: 'String',
+        },
+        state: {
+            type: 'String',
+        },
+        country: {
+            type: 'String',
+        },
+        zipcode: {
+            type: 'Number',
+        },
+        address: {
+            type: 'String',
+            required: true,
+        },
+    },
+    format: {
+        type: String,
+        required: true,
+    },
+    condition: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default Mongoose.model('Products', ProductSchema);
