8acc568b96a610d29547e5662965c40c4958cd1d
tejas96 <tejas96patil@gmail.com>
2021-11-29 19:06:25 +0530

    create graphql product server, create product dto layer

---
+++ packages/product-service/src/repository/productRepo.js
@@ -0,0 +1,82 @@
+import Product from '../models/Product';
+
+/**
+ *
+ * @param {String} id
+ * @returns
+ */
+export const getProductById = async (id) => {
+    return await Product.findById(id);
+};
+
+/**
+ *
+ * @returns
+ */
+export const getProducts = async () => {
+    return await Product.find();
+};
+
+/**
+ *
+ * @param {Object} product
+ * @returns
+ */
+export const createProduct = async (product) => {
+    return await Product.create(product);
+};
+
+/**
+ *
+ * @param {String} id
+ * @param {Object} product
+ * @returns
+ */
+export const updateProduct = async (id, product) => {
+    return await Product.findByIdAndUpdate(id, product, { new: true });
+};
+
+/**
+ *
+ * @param {String} id
+ * @returns
+ */
+export const deleteProduct = async (id) => {
+    return await Product.findByIdAndDelete(id);
+};
+
+/**
+ *
+ * @param {String} category
+ * @returns
+ */
+export const getProductsByCategory = async (category) => {
+    return await Product.find({ category });
+};
+
+/**
+ *
+ * @param {String} name
+ * @returns
+ */
+export const getProductsByName = async (name) => {
+    return await Product.find({ name });
+};
+
+/**
+ *
+ * @param {Number} price
+ * @returns
+ */
+export const getProductsByPrice = async (price) => {
+    return await Product.find({ price });
+};
+
+/**
+ *
+ * @param {String} description
+ * @returns
+ */
+export const getProductsByDescription = async (description) => {
+    return await Product.find({ description });
+};
