7a8b077b80285aa73babecac750322e48170837f
tejas96 <tejas96patil@gmail.com>
2021-12-01 23:12:14 +0530

    add firebase admin, add .graphql schema, add json scaler type

--- packages/product-service/src/config/firebaseAdmin.js
+++ packages/product-service/src/config/firebaseAdmin.js
@@ -1,8 +1,8 @@
-import admin from 'firebase-admin'
-import serviceAccountFile from './the-booktown-adminsdk.json'
+import admin from 'firebase-admin';
+import serviceAccountFile from './the-booktown-adminsdk.json';
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccountFile),
-})
+});
 
-export default admin
+export default admin;
