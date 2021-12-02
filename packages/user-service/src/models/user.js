ecafc120d456d1b7a4e6b1640b3f16a23c32ec2e
tejas96 <tejas96patil@gmail.com>
2021-12-02 19:01:06 +0530

    user serive flow completed

---
+++ packages/user-service/src/models/user.js
@@ -0,0 +1,25 @@
+import Mongoose from 'mongoose';
+
+const UserSchema = new Mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+    },
+});
+
+export default Mongoose.model('Users', UserSchema);
---
+++ packages/user-service/userService.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { ApolloServer, gql } from 'apollo-server-express';
+import fs from 'fs';
+import path from 'path';
+import resolveQuery from './src/controller';
+import GraphQLJSON from 'graphql-type-json';
+import { buildContext } from './src/utility/utilityFunctions';
+import firebaseAdmin from './src/config/firebaseAdmin';
+import { buildFederatedSchema } from '@apollo/federation';
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+mongoose
+    .connect(process.env.DB_CONNECTION_URL || '', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then((res) => {
+        console.log('DB connection successful');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+//function that imports .graphql files
+const importGraphQLAndGetSchema = (file) => {
+    const schema = fs.readFileSync(path.join(__dirname, file), 'utf-8');
+    return gql`
+        ${schema}
+    `;
+};
+
+const resolvers = { ...resolveQuery(), JSON: GraphQLJSON };
+
+async function createServer() {
+    const app = express();
+    const server = new ApolloServer({
+        schema: buildFederatedSchema([
+            {
+                typeDefs: importGraphQLAndGetSchema('./src/.graphql'),
+                resolvers,
+            },
+        ]),
+        context: buildContext,
+    });
+    await server.start();
+    server.applyMiddleware({ app });
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+createServer();
